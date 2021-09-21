68516a51b9620f0401ec169f1af9dce3f596f840
Karl Bristav <karl.bristav@gmail.com>
2021-09-21 06:43:39 +0200

    Add project files.

---
+++ AnturaTestsCSharp/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnturaTestsCSharp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
